3792f7a01a5a40699418a354cd5c121245127abc
Lucas <lucas.charles@kapturecrm.com>
2024-10-04 15:38:01 +0530

    Initial Commit

---
+++ KapChatBotSDK/LoggerService.swift
@@ -0,0 +1,14 @@
+//
+//  LoggerService.swift
+//  KapChatBotSDK
+//
+//  Created by Lucas Charles on 04/10/24.
+//
+
+import Foundation
+
+public class LoggerService {
+    public static func logString(for string: String) {
+        print("[LOGGER SERVICE] -> \(string)")
+    }
+}
